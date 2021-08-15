69a9368c75e0a1df516f95a046160ce4cec7e3b3
Gajendra Kumar <you@example.com>
2021-08-15 15:55:38 +0530

    Step 8

--- Step-8/index.js
+++ Step-8/index.js
@@ -1,10 +1,10 @@
 //Run npm run build:webpack1
 import add from "./math";
 
-//Debuggin woeking now using webpack
+//Debuggin working now using webpack
 const result = add(2, 2);
 
 result.then(value => {
-    console.log('OUTPUT', value);
+    console.log('OUTPUT2', value);
 })
 
